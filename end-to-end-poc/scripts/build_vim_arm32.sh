3a2309dfb16b55e99dd5a39cd11cb0798f1bcf2f
officeyutong <yt.xyxx@gmail.com>
2023-07-22 21:32:39 +0800

    Update vim cross build

--- end-to-end-poc/scripts/build_vim_arm32.sh
+++ end-to-end-poc/scripts/build_vim_arm32.sh
@@ -10,8 +10,8 @@ build_vim() {
     chmod 777 -R .
     make clean
     make distclean
-    vim_cv_toupper_broken=yes ./configure --enable-gui=no -disable-gtktest --disable-xim --disable-gpm --without-x --disable-netbeans --with-tlib=ncurses --host=arm-linux-gnueabihf --disable-sysmouse --disable-selinux --disable-icon-cache-update --disable-smack
-    # make CC=arm-linux-gnueabihf-gcc -j
+    LDFLAGS="-L/usr/local/ncurses-6.3/lib" vim_cv_getcwd_broken=no vim_cv_memmove_handles_overlap=yes vim_cv_stat_ignores_slash=yes vim_cv_tty_group=root vim_cv_tty_mode=0620  vim_cv_tgetent=zero vim_cv_terminfo=yes vim_cv_toupper_broken=yes ./configure --enable-gui=no -disable-gtktest --disable-xim --disable-gpm --without-x --disable-netbeans --with-tlib=ncurses --host=arm-linux-gnueabihf --disable-sysmouse --disable-selinux --disable-icon-cache-update --disable-smack
+    make -j
 }
 
 pushd ${self_dir}
