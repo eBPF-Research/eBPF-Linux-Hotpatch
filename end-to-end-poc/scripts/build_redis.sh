87b20b6d2bacff287ee772fe09a2d8cf3de1ec12
officeyutong <yt.xyxx@gmail.com>
2023-07-26 17:56:30 +0800

    Update redis building on arm32

--- end-to-end-poc/scripts/build_redis.sh
+++ end-to-end-poc/scripts/build_redis.sh
@@ -5,7 +5,19 @@ build_redis() {
     echo "Building redis-5.0-rc1 ..."
 
     cd ../redis-5.0-rc1
-    make MALLOC=libc -j
+    make distclean || true
+    MACHINE_TYPE=`uname -m`
+    if [[ ${MACHINE_TYPE} == 'x86_64' ||  ${MACHINE_TYPE} == 'aarch64' ]]; then
+        make MALLOC=libc -j
+    elif [[ ${MACHINE_TYPE} == 'i686' || ${MACHINE_TYPE} == 'i386' ]]; then
+        make MALLOC=libc -j 32bit
+    elif [[ ${MACHINE_TYPE} == 'armv7l' || ${MACHINE_TYPE} == 'armv6l' ]]; then
+        make MALLOC=libs CFLAGS="-march=native" FINAL_LIBS="-lm -latomic" -j 
+    else
+        echo "Unknown system type ${MACHINE_TYPE}"
+        exit 1
+    fi
+    
 }
 
 pushd ${self_dir}
