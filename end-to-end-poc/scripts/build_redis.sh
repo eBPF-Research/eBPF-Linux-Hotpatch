7f739812e49d445b77e6e5d035c061260c3d7ef9
officeyutong <yt.xyxx@gmail.com>
2023-07-26 18:39:27 +0800

    fix redis build on aarch64

--- end-to-end-poc/scripts/build_redis.sh
+++ end-to-end-poc/scripts/build_redis.sh
@@ -7,10 +7,12 @@ build_redis() {
     cd ../redis-5.0-rc1
     make distclean || true
     MACHINE_TYPE=`uname -m`
-    if [[ ${MACHINE_TYPE} == 'x86_64' ||  ${MACHINE_TYPE} == 'aarch64' ]]; then
+    if [[ ${MACHINE_TYPE} == 'x86_64' ]]; then
         make MALLOC=libc -j
     elif [[ ${MACHINE_TYPE} == 'i686' || ${MACHINE_TYPE} == 'i386' ]]; then
         make MALLOC=libc -j 32bit
+    elif [[  ${MACHINE_TYPE} == 'aarch64' ]]; then
+        make MALLOC=libs FINAL_LIBS="-lm -latomic" -j 
     elif [[ ${MACHINE_TYPE} == 'armv7l' || ${MACHINE_TYPE} == 'armv6l' ]]; then
         make MALLOC=libs CFLAGS="-march=native" FINAL_LIBS="-lm -latomic" -j 
     else
