5c3307ebb30e83ef2f0fd11a61a984570a9b5591
officeyutong <yt.xyxx@gmail.com>
2023-07-27 00:40:10 +0800

    update openssl buildscript

--- end-to-end-poc/scripts/build_openssl.sh
+++ end-to-end-poc/scripts/build_openssl.sh
@@ -7,9 +7,19 @@ build_openssl() {
 
     cd ../openssl-1.1.0b
     make distclean || true
-    ./Configure linux-aarch64 no-afalgeng --prefix=/usr/local/openssl-1.1.0b ${script_args[@]}
+    MACHINE_TYPE=`uname -m`
+    if [[ ${MACHINE_TYPE} == 'x86_64' ]]; then
+        ./Configure linux-x86_64 no-afalgeng --prefix=/usr/local/openssl-1.1.0b ${script_args[@]}
+    elif [[  ${MACHINE_TYPE} == 'aarch64' ]]; then
+        ./Configure linux-aarch64 no-afalgeng --prefix=/usr/local/openssl-1.1.0b ${script_args[@]}
+    elif [[ ${MACHINE_TYPE} == 'armv7l' || ${MACHINE_TYPE} == 'armv6l' ]]; then
+        ./Configure linux-armv4 no-afalgeng --prefix=/usr/local/openssl-1.1.0b ${script_args[@]}
+    else
+        echo "Unknown system type ${MACHINE_TYPE}"
+        exit 1
+    fi
+
     make -j
-    make install
 
 }
 
